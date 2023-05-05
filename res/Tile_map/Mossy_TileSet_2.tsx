c4dcb188768e289ff1e0f08e3f10ec9d0cb14cfc
hungdungn47 <hungdungn47@gmail.com>
2023-05-05 09:58:40 +0700

    add level 2

---
+++ res/Tile_map/Mossy_TileSet_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Mossy_TileSet_2" tilewidth="512" tileheight="512" tilecount="1" columns="1">
+ <image source="../Mossy_Tileset/Mossy_TileSet_2.png" width="560" height="560"/>
+</tileset>
